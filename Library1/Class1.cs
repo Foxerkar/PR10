8b57cf06898db79e33816c0ebc50db8b85fcc351
Ярослав Дмитриев < @42-15>
2024-10-16 11:35:53 +0500

    Добавьте файлы проекта.

---
+++ Library1/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library1
+{
+    public class Class1
+    {
+        public static (double Area, double Perimeter) Square(double Side)
+        {
+            double area = Side * Side;
+            double perimeter = 4 * Side;
+            return (area, perimeter);
+        }
+
+
+        public static (double Area, double Circus) Circle (double radius)
+        {
+            double area = Math.PI * radius * radius;
+            double circus = 2 * Math.PI * radius;
+            return (area, circus);
+        }
+
+    }
+}
---
+++ PR10/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Library1;
+
+namespace PR10
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(SquareDlina.Text, out double n))
+            {
+                double length = double.Parse(SquareDlina.Text);
+                var (area, perimeter) = Class1.Square(length);
+                SquareResult.Text = $"Площадь: {area}, Преиметр: {perimeter}";
+            }
+            else
+            {
+                MessageBox.Show("Введите корректное число.");
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(CircusRadius.Text, out double n))
+            {
+                double radius = double.Parse(CircusRadius.Text);
+                var (area, dlina) = Class1.Circle(radius);
+                CircusResult.Text = $"Площадь: {area}, Преиметр: {dlina}";
+            }
+            else
+            {
+                MessageBox.Show("Введите корректное число.");
+            }
+        }
+    }
+}
